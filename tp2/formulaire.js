7b9a9cc98624f1c47038048b5248da40eac7c78e
yasminebelaguid <yasminebelaguid@gmail.com>
2019-11-04 11:22:21 +0100

    yas1

---
+++ tp2/formulaire.js
@@ -0,0 +1,11 @@
+<script>
+Validation() {
+
+  if (document.getElementById('nom').length == 5){ 
+     alert ("Veuillez entrer un nom valide!");
+     error.innerHTML = "Nom invalide!"; }
+  
+}
+
+
+</script>
