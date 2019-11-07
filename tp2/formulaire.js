2681d5c5a512ea1c5d10a934430e46c2696f7713
yasminebelaguid <yasminebelaguid@gmail.com>
2019-11-07 23:52:57 +0100

    done

---
+++ tp2/formulaire.js
@@ -0,0 +1,55 @@
+
+
+function validation() {
+
+
+
+  this.event.preventDefault();
+  var test = true;
+  var text = "";
+
+
+
+
+    if (document.getElementById("nom").value.length < 5) 
+     
+	text = "Nom";
+    
+  
+     if (document.getElementById("prenom").value.length < 5) 
+   
+	text = text + ", Prénom";
+	
+
+     if (document.getElementById("add").value.length < 5) 
+   
+	text = text + ", Adresse";
+	
+
+     if (document.getElementById("email").value.length < 5) 
+      
+	text = text + ", Email";
+
+   
+     if (text.length == 0)
+	
+	test = false;
+    
+
+
+if (test) {
+
+   if (text.length > 8)
+
+   document.getElementById("error").innerHTML = "Les champs " + text + " doivent contenir au moins cinq caractères!";
+
+   else 
+
+   document.getElementById("error").innerHTML = "Le champ " + text + " doit contenir au moins cinq caractères!";}
+ 
+else
+
+   document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value + " 😀";
+
+
+}
