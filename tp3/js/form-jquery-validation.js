6bac364f42a8c31d4e17f268d10d0e9986ab247e
yasminebelaguid <yasminebelaguid@gmail.com>
2019-11-24 14:50:55 +0100

    done

---
+++ tp3/js/form-jquery-validation.js
@@ -0,0 +1,37 @@
+
+$( document ).ready(function() {
+
+    
+    console.log("DOM ready!");
+
+    var nom = $("#nom"); var prenom = $("#prenom"); var date = $("#date"); var add = $("#add"); var email = $("#email");
+
+    $("#valider").on("click", (event) => {
+
+        var test = (verifyInput(nom.val(), prenom.val(), date.val(), add.val(), email.val())) ? false : true;
+
+        console.log(test);
+
+        if (test) {
+            
+             $(".modal-title").html("Bienvenue " + prenom.val());
+           
+            $(".modal-body").html(" Vous êtes  nés le " + date.val()  
+            + " et vous habitez : " + ''
+            + '<a href="http://maps.google.com/maps?q=' + add.val() 
+            + '"> <img src="https://maps.googleapis.com/maps/api/staticmap?markers=' + add.val() 
+            + '&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg" alt="carte" style="width:100%">' + add.val() );
+
+        } else {
+
+            $(".modal-title").html("Erreur !"); $(".modal-body").html("Veuillez remplir tous les champs obligatoires..");
+        }
+
+            $("#myModal").modal("show");
+    });
+
+    verifyInput = (nom, prenom, date, add, email) => {
+        return nom == "" | prenom == "" | date == "" | add == "" | email == "";
+    }
+
+});
