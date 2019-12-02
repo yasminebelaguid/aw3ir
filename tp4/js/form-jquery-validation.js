104b856d6f07ce2a29cb00fb0d4413c74f47594b
yasminebelaguid <yasminebelaguid@gmail.com>
2019-12-02 21:46:36 +0100

    tp4

---
+++ tp4/js/form-jquery-validation.js
@@ -0,0 +1,61 @@
+
+
+$( document ).ready(function() {
+
+    
+    console.log("DOM ready!");
+
+
+   $("#gps").on("click", function () {
+        getLocation();
+    });
+
+   
+    $('#nom').keyup(function() {
+ 
+        // Le script qui devra calculer et afficher le nombre de caractères
+
+        var nombreCaractere = $(this).val().length;
+        
+        var msg =   nombreCaractere + ' car.';
+        $('#id').text(msg);
+      
+       });
+
+
+    $('#prenom').keyup(function () {
+
+        var nombreCaractere = $(this).val().length;
+
+        var msg = nombreCaractere + ' car.';
+        $('#idd').text(msg);
+
+    });
+
+
+ $("#valider").on("click", function ()
+    {
+
+
+    var 
+	nom = $("#nom"),
+        prenom = $("#prenom"),
+        date = $("#date"),
+        add = $("#add"),
+        email = $("#email");
+
+    contactStore.add(nom, prenom, date, add, email);
+
+    var list = contactStore.getList();
+    
+
+    for(var i in list){
+        list[i].name
+        $("table").append("<tr><td>"+list[i].name+"</td></tr>");
+    }
+
+
+});
+
+});
+
