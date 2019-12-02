ae4cb408d9901d2fb06e35cdf86e4bd8f9e29c96
yasminebelaguid <yasminebelaguid@gmail.com>
2019-12-02 21:55:12 +0100

    tp4done

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -1,24 +1,13 @@
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 34dfb1694b2670229231326c6f85e234d5d0fc47
 $( document ).ready(function() {
 
     
     console.log("DOM ready!");
-<<<<<<< HEAD
 
 
    $("#gps").on("click", function () {
         getLocation();
-=======
-    
-    
-    $('#gps').click(function () {
 
-        $('#map').html(getLocation());
->>>>>>> 34dfb1694b2670229231326c6f85e234d5d0fc47
     });
 
    
@@ -31,11 +20,8 @@ $( document ).ready(function() {
         var msg =   nombreCaractere + ' car.';
         $('#id').text(msg);
       
-<<<<<<< HEAD
+
        });
-=======
-       })
->>>>>>> 34dfb1694b2670229231326c6f85e234d5d0fc47
 
 
     $('#prenom').keyup(function () {
@@ -45,7 +31,7 @@ $( document ).ready(function() {
         var msg = nombreCaractere + ' car.';
         $('#idd').text(msg);
 
-<<<<<<< HEAD
+
     });
 
 
@@ -75,45 +61,4 @@ $( document ).ready(function() {
 
 });
 
-=======
-    })
 
-
-
-    var nom = $("#nom"); var prenom = $("#prenom"); var date = $("#date"); var add = $("#add"); var email = $("#email");
-
-
-    $("#valider").on("click", (event) => {
-
-        var test = (verifyInput(nom.val(), prenom.val(), date.val(), add.val(), email.val())) ? false : true;
-
-        console.log(test);
-
-        if (test) {
-
-
-            //contactStore.add(nom, prenom, date, add, email);
-            
-            $(".modal-title").html("Bienvenue " + prenom.val());
-           
-            $(".modal-body").html(" Vous êtes  nés le " + date.val()  
-            + " et vous habitez : " + ''
-            + '<a href="http://maps.google.com/maps?q=' + add.val() 
-            + '"> <img src="https://maps.googleapis.com/maps/api/staticmap?markers=' + add.val() 
-            + '&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg" alt="carte" style="width:100%">' + add.val() );
-
-        } else {
-
-            $(".modal-title").html("Erreur !"); $(".modal-body").html("Veuillez remplir tous les champs obligatoires..");
-        }
-
-            $("#myModal").modal("show");
-    });
-
-    verifyInput = (nom, prenom, date, add, email) => {
-        return nom == "" | prenom == "" | date == "" | add == "" | email == "";
-    }
-
-
-});
->>>>>>> 34dfb1694b2670229231326c6f85e234d5d0fc47
